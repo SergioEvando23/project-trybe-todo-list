46cb0f2573a8484a77539a0c53cd8d1dbf7bcecd
SergioEvando23 <sergioevandocosta@gmail.com>
2021-11-27 17:03:37 -0300

    adiocionado validação pra saber se ja tem a classe selected

--- script.js
+++ script.js
@@ -2,13 +2,23 @@ const button = document.getElementById('criar-tarefa');
 const input = document.getElementById('texto-tarefa');
 const orderList = document.getElementById('lista-tarefas');
 
-button.addEventListener('click', () =>{
-    const itemList = document.createElement('li');   
+function removeBackground() {
+    for (let ind = 0; ind < orderList.children.length; ind += 1) {
+        orderList.children[ind].classList.remove('selected');
+    }
+}
+
+button.addEventListener('click', () => {
+    const itemList = document.createElement('li');
     orderList.appendChild(itemList);
     itemList.innerText = input.value;
     input.value = "";
 });
 
 orderList.addEventListener('click', (event) => {
+    removeBackground();
     event.target.classList.toggle('selected');
 })
+
+
+
